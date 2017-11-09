f5cec5836cc6c2e20bcb453178e7c2a95d70d416
Toranian <mediamanmorrow@gmail.com>
2017-11-09 12:29:13 -0800

    Initial commit

---
+++ Main.py
@@ -0,0 +1,41 @@
+import os, sys, time
+import pygame
+from settings import *
+
+# Game display: 6 | Title: 7 | Game Exit: 8 | Initialization: 9-10
+# Lead X & Y |
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Slither")
+gameExit = False
+init = pygame.init()
+print("{} Successes, {} Failures".format(init[0], init[1]))
+
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+
+clock = pygame.time.Clock()
+
+# Game loop
+while not gameExit:
+    # Event handler | Quit: 16 |
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+
+        # Key events
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                lead_x_change -= 10
+            if event.key == pygame.K_d:
+                lead_x_change += 10
+
+    lead_x += lead_x_change
+    gameDisplay.fill(WHITE)
+    pygame.draw.rect(gameDisplay, BLACK, [lead_x, lead_y, 10, 10])
+    pygame.display.update()
+
+    clock.tick(30)
+
+pygame.quit()
+quit()
---
+++ settings.py
@@ -0,0 +1,14 @@
+# Settings for the game
+
+TITLE = "George"
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+# Define colors
+WHITE = (240, 240, 240)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
